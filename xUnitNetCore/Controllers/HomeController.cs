434be46e7a48a60afab8cd7ace769652376dce66
Danilov <albert95danilov@gmail.com>
2023-09-06 18:44:36 +0300

    Добавьте файлы проекта.

---
+++ xUnitNetCore/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using xUnitNetCore.Models;
+
+namespace xUnitNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello world!";
+            return View("Index");
+        }
+    }
+}
